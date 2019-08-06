7d635503fef31ad50397cbd46f21ec6a81ab2c36
Rahul Kumar <thedatatot@gmail.com>
2019-08-06 09:11:22 +0530

    R cLASS 2

--- R/2.R
+++ R/2.R
@@ -22,3 +22,57 @@ switch(choice,
        num.a - num.b,
        num.a * num.b,
        num.a / num.b)
+
+# loops
+
+counter = 0
+table.of = 9
+
+while (counter <= 10) {
+  counter = counter + 1
+  if(counter == 5){
+    next
+  }
+  if(counter == 9){
+    break
+  }
+  print(table.of * counter)
+}
+
+counter2 = 0
+while(counter2 <= 99){
+  counter2 = counter2 + 1
+  if(counter2 %% 2 == 0){
+    next
+  }else {
+    print(counter2)
+  }
+}
+
+my.vec <- c(9,4,0,9,2,4,5,6,2,3,0,9,4,6,9,8,2,3,5,0,9,7)
+for(each in my.vec){
+  print(each)
+}
+
+
+# Fibonacci Series
+terms = 100
+num1 = 0
+num2 = 1
+count = 2
+  
+if(terms == 1) {
+  print("Fibonacci Number:")
+  print(num1)
+} else {
+  print("Fibonacci Number:")
+  print(num1)
+  print(num2)
+  while(count < terms) {
+    nth = num1 + num2
+    print(nth)
+    num1 = num2
+    num2 = nth
+    count = count + 1
+  }
+}
